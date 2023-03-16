07da267c8d961a8a2b562f847886d515bd7e5a69
Favour-Yusuf <favouryusuf45@gmail.com>
2023-03-16 17:04:26 +0100

    update

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Footer from "./LandingPage/Footer";
 import Hero from "./LandingPage/Hero";
+import ManageAccount from "./LandingPage/ManageAccount";
 
 const HomeScreen = () => {
-    return <div>
-      <Hero/>
-  </div>;
+  return (
+    <div>
+      <Hero />
+      <Footer />
+      <ManageAccount />
+    </div>
+  );
 };
 
 export default HomeScreen;
---
+++ src/components/LandingPage/ManageAccount.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import styled from "styled-components";
+
+const ManageAccount = () => {
+  return (
+    <div>
+      <Container>
+        <Wrapper>
+          <Left>
+            <h1>A waste management platform that serves you</h1>
+          </Left>
+        </Wrapper>
+      </Container>
+    </div>
+  );
+};
+
+export default ManageAccount;
+
+const Left = styled.div`
+  width: 100%;
+  /* height: 20vh; */
+  display: flex;
+  align-items: center;
+  margin-top: 30px;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
+
+  h1 {
+    width: 35%;
+    margin: 0;
+    font-size: 2.5rem;
+    font-weight: 600;
+    text-transform: capitalize;
+
+    @media screen and (max-width: 768px) {
+      width: 80%;
+      font-size: 2.2rem;
+    }
+  }
+`;
+
+const Wrapper = styled.div`
+  width: 95%;
+  margin: auto;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Container = styled.div`
+  width: 100%;
+  color: #00002d;
+`;
