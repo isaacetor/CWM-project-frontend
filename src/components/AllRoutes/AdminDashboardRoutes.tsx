16c89818c970b7f5ea536aa19048fa52baa2d1dd
Ajinung <isaacetor7@gmail.com>
2023-03-16 13:19:23 +0100

    added the auth page and browser router in the index

---
+++ src/components/AllRoutes/AdminDashboardRoutes.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useNavigate, useRoutes } from "react-router-dom";
+import styled from "styled-components";
+import AdminDashboardHeader from "../Dashboard/AdminDashboard/AdminDashboardHeader";
+import AdminDashboardHome from "../Dashboard/AdminDashboard/AdminDashboardHome";
+
+const AdminDashboardRoutes = () => {
+  const element = useRoutes([
+    {
+      path: "/admin",
+      element: <AdminDashboardHome />,
+    },
+  ]);
+
+  return (
+    <div>
+      <Container>
+        <AdminDashboardHeader />
+        {element}
+      </Container>
+    </div>
+  );
+};
+
+export default AdminDashboardRoutes;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
