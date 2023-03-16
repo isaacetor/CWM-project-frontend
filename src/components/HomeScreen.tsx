b309fe2190f6457da28bbb254bbee6aa5e43c461
Favour-Yusuf <favouryusuf45@gmail.com>
2023-03-16 17:07:17 +0100

    update

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./LandingPage/Footer";
 import Hero from "./LandingPage/Hero";
 import ManageAccount from "./LandingPage/ManageAccount";
+import TrustedBy from "./LandingPage/TrustedBy";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +10,7 @@ const HomeScreen = () => {
       <Hero />
       <Footer />
       <ManageAccount />
+      <TrustedBy />
     </div>
   );
 };
