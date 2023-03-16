7363d307d4bfe8907f76a7275de1cfb734b14300
Ajinung <isaacetor7@gmail.com>
2023-03-16 16:43:36 +0100

    ADDED dashbord

--- src/components/Dashboard/UserDashboard/UserDashboardSidebar.tsx
+++ src/components/Dashboard/UserDashboard/UserDashboardSidebar.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
 const UserDashboardSidebar = () => {
-  return <div>UserDashboardSidebar</div>;
+  return (
+    <div>
+      <Container></Container>
+    </div>
+  );
 };
 
 export default UserDashboardSidebar;
+
+const Container = styled.div`
+  width: 300px;
+  height: 100vh;
+  background-color: #03001c;
+`;
