b7559a1ae3baa51329dc338ef01eeb26f9b990c5
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-17 05:12:03 +0100

    gotten one client

--- src/components/AllRoutes/HomeRoutes.tsx
+++ src/components/AllRoutes/HomeRoutes.tsx
@@ -32,7 +32,7 @@ const HomeRoutes = () => {
   ]);
   return (
     <div>
-      {selector?.name ? null : <Header />}
+      {/* {selector?.name ? null : <Header />} */}
 
       {element}
     </div>
--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Features from "./LandingPage/Features";
 import Footer from "./LandingPage/Footer";
 import GetStarted from "./LandingPage/GetStarted";
+import Header from "./LandingPage/Header";
 import Hero from "./LandingPage/Hero";
 import ManageAccount from "./LandingPage/ManageAccount";
 import Profile from "./LandingPage/Profile";
@@ -10,6 +11,7 @@ import TrustedBy from "./LandingPage/TrustedBy";
 const HomeScreen = () => {
   return (
     <div style={{ overflow: "hidden" }}>
+      <Header />
       <Hero />
       <ManageAccount />
       <TrustedBy />
